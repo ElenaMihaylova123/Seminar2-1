05401c7d6612af2426d50b47342ab205e6e20576
ElenaMihaylova <elena.16.04@mail.ru>
2023-05-06 17:17:39 +0700

    Выполнены задачи 2,3,6,8

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 4. 
+//Напишите программу, принимает на вход три числа и выводит максимальное из них 
+
+
+Console.WriteLine("Введи 3 числа через пробел");
+string[] i = Console.ReadLine().Split(' ');
+int a = Convert.ToInt32(i[0]);
+int b = Convert.ToInt32(i[1]);
+int c = Convert.ToInt32(i[2]);
+Console.WriteLine(Math.Max(a, Math.Max(b, c)));
---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 6
+//Вывести все чётные числа от 1 до N. N задаётся с клавиатуры.
+
+
+Console.WriteLine("Введи число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i=0; i<=N; i++) {
+   if(i%2 == 0) {
+    Console.Write("{0} ", i);
+   }
+  }
